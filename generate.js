9c5c1bce0f475e2807e7f69958e1a084b9368aab
淡航 <m15274906257@163.com>
2020-11-20 00:16:41 +0800

    feat: 基本实现, 可通过node generate 生成

--- generate.js
+++ generate.js
@@ -1,18 +1,20 @@
 const fs = require('fs');
+const moment = require('moment')
 
 let basePath = 'src/components';
 const cptName = process.argv.splice(2)[0] // 创建的文件名
 let path = cptName.split('/');
 let fileName = path[path.length - 1];
 let readsFile = ['src/tmp/index.js','src/tmp/tmp.js']
-let writeFile = [`${fileName}.js`, `${fileName}.html`, `${fileName}.css`, 'index.js' ]
+let writeFiles = [`${fileName}.js`, `${fileName}.html`, `${fileName}.css`, 'index.js' ]
+let files = []
 
 // 检测是否存在文件夹
-const exists = function () {
+const checkFileExists = function () {
     return new Promise((res, reject) => {
         (async function () {
             for (let a of path) {
-                fs.existsSync(basePath + a) ? basePath = `${basePath}${a}/` : await mkdir(a)
+                fs.existsSync(basePath + '/' + a) ? basePath = `${basePath}/${a}/` : await mkdir(a)
             }
             res(basePath)
         })()
@@ -22,9 +24,9 @@ const exists = function () {
 // 建立文件夹
 let mkdir = function(dir) {
     return new Promise((res, rej) => {
-        fs.mkdir(basePath + dir, (err) => {
+        fs.mkdir(basePath + '/' + dir, (err) => {
             if (err) rej(err);
-            basePath = `${basePath}${dir}`
+            basePath = `${basePath}/${dir}`
             res(basePath)
         })
     })
@@ -36,24 +38,37 @@ let readFile = function() {
         for (let file of readsFile) {
             let text = fs.readFileSync(file).toString();
             text = text.replace(/time/g, moment().format('YYYY/MM/DD'))
-                .replace(/temp/g, name).replace(/author/g, author)
-            file.push(text)
+                .replace(/temp/g, fileName)
+            files.push(text)
         }
-        res(file);
+        res(files);
     })
 }
 
 // 写入文件
-let writeFile = function(file) {
+let writeToFile = function(file) {
     return new Promise((res, rej) => {
         (async function () {
-            for (let a of writeFile) {
-                await fs.write(`${basePath}/${a}`,
-                    a == writeFile[3] ? file[0] : a == writeFile[0] ? file[1] : '', (err) => {
+            for (let a of writeFiles) {
+                await fs.writeFile(`${basePath}/${a}`,
+                    a === writeFiles[3] ? file[0] : a === writeFiles[0] ? file[1] : '', (err) => {
                         if (err) rej(err)
                     })
             }
-            res('succ')
+            res('success')
         })()
     })
 }
+
+async function generateComponent() {
+    try {
+        await checkFileExists();
+        let files = await readFile();
+        await writeToFile(files);
+        console.log(`已成功创建${fileName}模板`)
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+generateComponent()
